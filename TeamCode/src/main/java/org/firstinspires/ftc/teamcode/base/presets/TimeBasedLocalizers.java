229d516f0f1ec3efffccc4b98e0da44f85e80cc7
roobootics <arick@solarflarerobotics.org>
2025-05-10 19:06:08 -0400

    skoobedi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/presets/TimeBasedLocalizers.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/presets/TimeBasedLocalizers.java
@@ -13,6 +13,7 @@ public abstract class TimeBasedLocalizers{
         public double ABS_SERVO_SPEED;
         public double prevPosition;
         public double prevTime;
+        public double prevTarget;
         public Components.BotServo actuator;
         public ServoTimeBasedLocalizer(double servoSpeed,Components.BotServo actuator){
             this.ABS_SERVO_SPEED=servoSpeed;
@@ -20,16 +21,17 @@ public abstract class TimeBasedLocalizers{
         }
         public double getCurrentPosition(Servo servo){
             if (!Double.isNaN(servo.getPosition())){
-                double servoSpeed = Math.signum(actuator.positionConversionInverse.apply(actuator.getPosition())-prevPosition)*ABS_SERVO_SPEED;
+                double servoSpeed = Math.signum(prevTarget-prevPosition)*ABS_SERVO_SPEED;
                 double time=timer.time();
                 double change = actuator.positionConversionInverse.apply(servoSpeed)*(time-prevTime);
                 if (Math.signum(change)==1){
-                    prevPosition=Math.min(actuator.positionConversionInverse.apply(actuator.getPosition()),prevPosition+change);
+                    prevPosition=Math.min(prevTarget,prevPosition+change);
                 }
                 else if (Math.signum(change)==-1){
-                    prevPosition=Math.max(actuator.positionConversionInverse.apply(actuator.getPosition()),prevPosition+change);
+                    prevPosition=Math.max(prevTarget,prevPosition+change);
                 }
                 prevTime=time;
+                prevTarget=actuator.positionConversionInverse.apply(actuator.getPosition());
             }
             return prevPosition;
         }
@@ -38,14 +40,16 @@ public abstract class TimeBasedLocalizers{
         public double ABS_SERVO_SPEED;
         public double prevPosition;
         public double prevTime;
+        public double prevPower;
         public CRTimeBasedLocalizer(double servoSpeed){
             this.ABS_SERVO_SPEED=servoSpeed;
         }
         public double getCurrentPosition(E actuator){
-            double servoSpeed = actuator.getPower()*ABS_SERVO_SPEED;
+            double servoSpeed = prevPower*ABS_SERVO_SPEED;
             double time=timer.time();
             prevPosition=prevPosition+servoSpeed*(time-prevTime);
             prevTime=time;
+            prevPower=actuator.getPower();
             return prevPosition;
         }
     }
