c7247ce75ce56630e4b0852afdcf0b967c3015bb
roobootics <arick@solarflarerobotics.org>
2025-08-29 22:19:54 -0400

    migration to Pedro 2.0.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,18 +1,63 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
-
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+public class Constants {
+
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(5.624545)
+            .forwardZeroPowerAcceleration(-36.1946)
+            .lateralZeroPowerAcceleration(-55.2887)
+            .centripetalScaling(0.001);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .leftFrontMotorName("leftFront")
+            .leftRearMotorName("leftRear")
+            .rightFrontMotorName("rightFront")
+            .rightRearMotorName("rightRear")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(67.0896)
+            .yVelocity(57.3758);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(7.875)
+            .strafePodX(0)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(
+                    GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD
+            )
+            .customEncoderResolution(13.26291192)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+    public static PathConstraints pathConstraints = new PathConstraints(
+            0.95,
+            0.1,
+            0.1,
+            0.007,
+            50,
+            6.8,
+            10,
+            1
+    );
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .build();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Pedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Pedro.java
@@ -1,31 +1,24 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.pedropathing.follower.Follower;
-import com.pedropathing.localization.Pose;
-import com.pedropathing.pathgen.PathBuilder;
-import com.pedropathing.pathgen.PathChain;
-import com.pedropathing.pathgen.Point;
-import com.pedropathing.util.Constants;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathBuilder;
+import com.pedropathing.paths.PathChain;
 
 import org.firstinspires.ftc.teamcode.base.Components;
 import org.firstinspires.ftc.teamcode.base.LambdaInterfaces;
 import org.firstinspires.ftc.teamcode.base.Commands;
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.function.Function;
 
 public abstract class Pedro {
     static{
-        Constants.setConstants(FConstants.class, LConstants.class);
         PedroSleepUntilPose.setGetPose(()->{
             Pose pose=getPose();
             return new double[]{pose.getX(),pose.getY(),pose.getHeading()};
         });
     }
-    public static final Follower follower=Constants.createFollower();
+    public static final Follower follower=Constants.createFollower(Components.getHardwareMap());
     private final static LambdaInterfaces.ReturningFunc<Pose> getPose = new Components.CachedReader<>(
             ()->{follower.updatePose(); return follower.getPose();},
             1
@@ -33,9 +26,6 @@ public abstract class Pedro {
 
     public static Pose getPose(){
         return getPose.call();
-    }
-    public static void setStartingPose(Pose pose){
-        follower.poseUpdater.setStartingPose(pose);
     }
     public static Commands.RunResettingLoop updateCommand(){
         return new Commands.RunResettingLoop(new Commands.InstantCommand(follower::update));
@@ -62,7 +52,7 @@ public abstract class Pedro {
         public PedroLinearCommand(double x, double y, double heading, boolean holdEnd){ //Goes to the position indicated by the inputs
             super(
                     (PathBuilder b)-> b
-                            .addBezierLine(new Point(follower.getPose()),new Point(x,y))
+                            .addPath(new BezierLine(follower.getPose(),new Pose(x,y)))
                             .setLinearHeadingInterpolation(follower.getPose().getHeading(),heading),
                     holdEnd
             );
@@ -72,12 +62,9 @@ public abstract class Pedro {
         public PedroLinearChainCommand(boolean holdEnd, Pose...poses){ //In a path chain, goes to all positions provided one at a time
             super(
                     (PathBuilder b)->{
-                            ArrayList<Pose> poseList = new ArrayList<>();
-                            poseList.add(follower.getPose());
-                            Collections.addAll(poseList,poses);
-                            for (int i=0;i<poses.length;i++){
-                                b.addBezierLine(new Point(poseList.get(i)),new Point(poseList.get(i+1)));
-                                b.setLinearHeadingInterpolation(poseList.get(i).getHeading(),poseList.get(i+1).getHeading());
+                            for (Pose pose : poses) {
+                                b.addPath(new BezierLine(follower::getPose, pose));
+                                b.setLinearHeadingInterpolation(pose.getHeading(), pose.getHeading());
                             }
                             return b;
                     },
@@ -89,7 +76,7 @@ public abstract class Pedro {
         public PedroLinearTransformCommand(double x, double y, double heading, boolean holdEnd){ //Transforms from current position by the given inputs
             super(
                     (PathBuilder b)-> b
-                            .addBezierLine(new Point(follower.getPose()),new Point(follower.getPose().getX()+x,follower.getPose().getY()+y))
+                            .addPath(new BezierLine(follower.getPose(),new Pose(follower.getPose().getX()+x,follower.getPose().getY()+y)))
                             .setLinearHeadingInterpolation(follower.getPose().getHeading(),follower.getPose().getHeading()+heading),
                     holdEnd
             );
@@ -103,7 +90,7 @@ public abstract class Pedro {
     public static class PedroInstantLinearCommand extends PedroInstantCommand{
         public PedroInstantLinearCommand(double x, double y, double heading, boolean holdEnd) {
             super((PathBuilder b)-> b
-                            .addBezierLine(new Point(follower.getPose()),new Point(x,y))
+                            .addPath(new BezierLine(follower.getPose(),new Pose(x,y)))
                             .setLinearHeadingInterpolation(follower.getPose().getHeading(),heading),
                     holdEnd
             );
@@ -113,12 +100,9 @@ public abstract class Pedro {
         public PedroInstantLinearChainCommand(boolean holdEnd, Pose...poses){ //In a path chain, goes to all positions provided one at a time
             super(
                     (PathBuilder b)->{
-                        ArrayList<Pose> poseList = new ArrayList<>();
-                        poseList.add(follower.getPose());
-                        Collections.addAll(poseList,poses);
-                        for (int i=0;i<poses.length;i++){
-                            b.addBezierLine(new Point(poseList.get(i)),new Point(poseList.get(i+1)));
-                            b.setLinearHeadingInterpolation(poseList.get(i).getHeading(),poseList.get(i+1).getHeading());
+                        for (Pose pose : poses) {
+                            b.addPath(new BezierLine(follower::getPose, pose));
+                            b.setLinearHeadingInterpolation(pose.getHeading(), pose.getHeading());
                         }
                         return b;
                     },
@@ -129,7 +113,7 @@ public abstract class Pedro {
     public static class PedroInstantLinearTransformCommand extends PedroInstantCommand{
         public PedroInstantLinearTransformCommand(boolean holdEnd, double x, double y, double heading) {
             super((PathBuilder b)-> b
-                            .addBezierLine(new Point(follower.getPose()),new Point(follower.getPose().getX()+x,follower.getPose().getY()+y))
+                            .addPath(new BezierLine(follower.getPose(),new Pose(follower.getPose().getX()+x,follower.getPose().getY()+y)))
                             .setLinearHeadingInterpolation(follower.getPose().getHeading(),follower.getPose().getHeading()+heading),
                     holdEnd
             );
