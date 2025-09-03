aab1fb32628cdef02950c50e5d9abde1f02e704f
roobootics <arick@solarflarerobotics.org>
2025-09-02 21:44:12 -0400

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Pedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Pedro.java
@@ -13,20 +13,13 @@ import org.firstinspires.ftc.teamcode.base.LambdaInterfaces;
 import java.util.function.Function;
 
 public abstract class Pedro {
+    public static Follower follower;
+
     static{
         PedroSleepUntilPose.setGetPose(()->{
-            Pose pose=getPose();
+            Pose pose=follower.getPose();
             return new double[]{pose.getX(),pose.getY(),pose.getHeading()};
         });
-    }
-    public static Follower follower;
-    private static final LambdaInterfaces.ReturningFunc<Pose> getPose = new Components.CachedReader<>(
-            ()->{follower.updatePose(); return follower.getPose();},
-            1
-    )::cachedRead;
-
-    public static Pose getPose(){
-        return getPose.call();
     }
     public static void createFollower(Pose startingPose){
         follower=Constants.createFollower(Components.getHardwareMap());
